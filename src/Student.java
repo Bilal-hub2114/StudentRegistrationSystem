f99dc9e574b299897b7926c4c65d54a2f6b58cdd
Bilal Ay <aybilal2340@gmail.com>
2025-12-12 01:17:43 +0300

    "Öğrenci sınıfı eklendi."

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+    private String studentName;
+    private String studentSurname;
+    private int studentID;
+    public Student(String studentName,String studentSurname,int studentID){
+        this.studentName=studentName;
+        this.studentSurname=studentSurname;
+    }
+}
